2936536b2268a3f14a6c4e70ca2b2803bdc41494
Hasnat Amin <62244945+hasnat7amin@users.noreply.github.com>
2023-09-22 16:05:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const server = http.createServer(app);
 app.use(express.json());
 app.use(cors());
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", function (req, res) {
   return res.send("Welcome to the apis");
