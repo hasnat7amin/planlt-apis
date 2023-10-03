46dc600af0c797ee5640c1afe5d0ddf9d8934226
Hasnat Amin <62244945+hasnat7amin@users.noreply.github.com>
2023-10-03 08:42:45 +0500

    Update verify-code.js

--- controllers/auth/verify-code.js
+++ controllers/auth/verify-code.js
@@ -1,6 +1,8 @@
 const OTP = require("../../models/OTP");
 const Users = require("../../models/Users");
 const sendErrorResponse = require("../../utils/send-error-response")
+const createToken = require("../../utils/create-token");
+
 
 module.exports = async (req, res) => {
     try {
@@ -12,12 +14,16 @@ module.exports = async (req, res) => {
         let user = await Users.findById(Otp.userId);
         user.isVerified = true;
         await user.save();
+        const token = await createToken(user._id);
         return res.status(200).json({
             code : 200,
             status: true,
             message: "Successfully verified OTP.",
             result: {
                 userId: Otp.userId,
+                user: user,
+                token:token
+
             }
         })
 
