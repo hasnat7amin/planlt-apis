7957f69675bbc6bb953804fb2ac0d2e257c8f098
Hasnat Amin <hasnatamin065@gmail.com>
2024-01-24 19:39:27 +0500

    Update profile_routes.js

--- routes/profile_routes.js
+++ routes/profile_routes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.put("/change-password", authorize, AdminController.ChangePassword)
 router.put("/change-information", authorize, AdminController.PersonalInfo)
 router.put("/change-image", authorize,upload.single("image"),authorize,AdminController.ChangeImage)
-router.get("/create/subscription", authorize,AdminController.CreateSubscrption)
+router.get("/create/subscription/:type", authorize,AdminController.CreateSubscrption)
 router.get("/:userId/subscriptions/success",AdminController.ConfirmSubscription)
 router.get("/cancel/subscription", admin_authorize,AdminController.CancelSubscription)
 router.get('/user-info',authorize, AdminController.GetPersonalInfo)
